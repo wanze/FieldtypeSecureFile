c339dc185759ed6fff0b2c8e1dfa41cb543ceaf5
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2015-08-14 09:41:20 +0200

    Some additional overwrites in the PagefilesSecure class

--- PagefilesSecure.php
+++ PagefilesSecure.php
@@ -10,10 +10,46 @@ class PagefilesSecure extends Pagefiles
 {
 
     /**
+     * @param mixed $item
+     * @return bool
+     */
+    public function isValidItem($item)
+    {
+        return ($item instanceof PagefileSecure);
+    }
+
+
+    /**
+     * Add a new PagefileSecure item, or create one from it's filename and add it.
+     *
+     * @param PagefileSecure|string $item If item is a string (filename) then the PagefileSecure instance will be created automatically.
+     * @return $this
+     */
+    public function add($item)
+    {
+        if (is_string($item)) {
+            $item = new PagefileSecure($this, $item);
+        }
+
+        return parent::add($item);
+    }
+
+
+    /**
      * @return string
      */
     public function path()
     {
         return $this->page->filesManager->securePath($this->field);
     }
+
+
+    /**
+     * @return PagefileSecure
+     */
+    public function makeBlankItem()
+    {
+        return new PagefileSecure($this, '');
+    }
+
 }
